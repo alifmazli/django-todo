95d82153b35385ff697aab7c1101f176f6343fc0
Alif <alifmazli01@gmail.com>
2022-02-21 17:12:45 +0800

    added task-create logic

--- base/views.py
+++ base/views.py
@@ -1,7 +1,9 @@
 from asyncio import Task
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Task
 
 class TaskList(ListView):
@@ -14,3 +16,8 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
