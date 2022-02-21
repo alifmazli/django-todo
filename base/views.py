5cb4d1f810d5637474d607bc4c0e04fda99797dd
Alif <alifmazli01@gmail.com>
2022-02-21 22:52:47 +0800

    added delete task and login/logout logic

--- base/views.py
+++ base/views.py
@@ -3,10 +3,21 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Task
 
-class TaskList(ListView):
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+
+class TaskList(LoginRequiredMixin, ListView):
     # looks for task_list.html
     model = Task
     context_object_name = 'tasks'
@@ -27,4 +38,10 @@ class TaskUpdate(UpdateView):
     # looks for task_form.html
     model = Task
     fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    # looks for task_confirm_delete.html
+    model = Task
+    context_object_name = 'task'
     success_url = reverse_lazy('tasks')
