be0cdd9f862e10be4321059b6eae313a32e36ce0
Alif <alifmazli01@gmail.com>
2022-02-22 00:54:03 +0800

    added registration flow

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import CustomLoginView, TaskCreate, TaskDelete, TaskList, TaskDetail, TaskUpdate
+from .views import CustomLoginView, TaskCreate, TaskDelete, TaskList, TaskDetail, TaskUpdate, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('register/', RegisterPage.as_view(), name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
